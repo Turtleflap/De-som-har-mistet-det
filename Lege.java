ae3fd882fcc7d84408f2f2c18270310099cb2b5f
Henrik <henrikcmt@gmail.com>
2022-03-14 23:01:11 +0100

    Gjort ferdig

--- Lege.java
+++ Lege.java
@@ -1,6 +1,6 @@
 public class Lege implements Comparable<Lege> {
     protected String navn;
-    IndeksertListe<Resept> utskrevneResepter;
+    Lenkeliste<Resept> utskrevneResepter;
 
     public Lege(String navn){
         this.navn = navn;
@@ -12,6 +12,42 @@ public class Lege implements Comparable<Lege> {
     public Lenkeliste<Resept> hentResept() {
         return utskrevneResepter;
     }
+
+
+    // 4 metoder som oppretter alle resept typene
+    
+    public HvitResept skrivHvitResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+        // this er objektet av Lege
+        HvitResept hvitResept = new HvitResept(legemiddel, this, pasient, reit);
+        // legger til utskrevne og pasient resepter
+        // Trenger å caste til Resept for leggTil() metoden
+        utskrevneResepter.leggTil((Resept) hvitResept);
+        pasient.leggTilResept(hvitResept);
+        // returnerer
+        return hvitResept;
+    }
+
+    public Militaerresept skrivMilResept (Legemiddel legemiddel, Pasient pasient) throws UlovligUtskrift {
+        Militaerresept militaerresept = new Militaerresept(legemiddel, this, pasient);
+        utskrevneResepter.leggTil((Resept) militaerresept);
+        pasient.leggTilResept(militaerresept);
+        return militaerresept;
+    }
+
+    public PResept skrivPResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+        PResept pResept = new PResept(legemiddel, this, pasient, reit);
+        utskrevneResepter.leggTil((Resept) pResept);
+        pasient.leggTilResept(pResept);
+        return pResept;
+    }
+
+    public BlaaResept skrivBlaaResept (Legemiddel legemiddel, Pasient pasient, int reit) throws UlovligUtskrift {
+        BlaaResept blaaResept = new BlaaResept(legemiddel, this, pasient, reit);
+        utskrevneResepter.leggTil((Resept) blaaResept);
+        pasient.leggTilResept(blaaResept);
+        return blaaResept;
+    }
+    
 
     @Override
     public String toString() {
