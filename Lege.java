45925772ff8a3f3c604615d85ea5e19503cfec61
Henrik <henrikcmt@gmail.com>
2022-03-14 22:23:47 +0100

    Mangler D3

--- Lege.java
+++ Lege.java
@@ -1,5 +1,6 @@
-public class Lege {
+public class Lege implements Comparable<Lege> {
     protected String navn;
+    IndeksertListe<Resept> utskrevneResepter;
 
     public Lege(String navn){
         this.navn = navn;
@@ -7,8 +8,20 @@ public class Lege {
     public String hentNavn() {
         return navn;
     }
+
+    public Lenkeliste<Resept> hentResept() {
+        return utskrevneResepter;
+    }
+
     @Override
     public String toString() {
         return "Navn: " + navn;
+    }
+    @Override
+    // returner en int
+    // Bruker mindre enn (A < B) for å få det alfabetisk
+    // https://www.codegrepper.com/code-examples/java/how+to+check+if+one+string+is+alphabetically+before+another+in+java
+    public int compareTo(Lege o) {
+        return this.navn.compareTo(o.navn);
     }
 }
