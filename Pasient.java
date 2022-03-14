c23d4cdf0f91acbc12b872921fed786941c5489d
Turtleflap <henrikcmt@gmail.com>
2022-03-14 17:07:28 +0100

    mangler hentResepter()

--- Pasient.java
+++ Pasient.java
@@ -1,3 +1,42 @@
 public class Pasient {
+    private String navn;
+    private String fodselsnummer;
+    private int ID;
+    // Må være static fordi det er felles for alle objekter
+    static int ID_teller = 1;
+
+    public Pasient(String navn, String fodselsnummer) {
+        this.navn = navn;
+        this.fodselsnummer = fodselsnummer;
+        // setter ID lik ID_teller og øker med 1
+        ID = ID_teller;
+        ID_teller++;
+    }
+
+    public void leggTilResept(Respet resept) {
+        // legg til metode her fra resept klassen
+    }
+
+    public hentResepter() {
+        // legg til Resepter her
+        return resepter;
+    }
+
+    public String hentNavn() {
+        return navn;
+    }
     
+    public String hentFodselsnummer() {
+        return fodselsnummer;
+    }
+
+    public int hentID() {
+        return ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasient med navn: " + navn + ". Fodselsnummer: " + 
+        fodselsnummer + ". ID: " + ID + ". Resepter: " + resepter;
+    }
 }
