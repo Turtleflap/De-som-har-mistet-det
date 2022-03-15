ca550a23e09308f508ffc34ed57cd8fb1d281eb5
Henrik <henrikcmt@gmail.com>
2022-03-15 12:56:39 +0100

    La til noen ekstra tester

--- testLege.java
+++ testLege.java
@@ -17,18 +17,22 @@ public class testLege {
         Pasient pasient = new Pasient("Karl", "12345612345");
 
         //tester comparable
-        System.out.println("tester comparable");
+        System.out.println("tester comparable verdier:");
         System.out.println(lege.compareTo(spesialist));
         System.out.println(spesialist.compareTo(lege));
-        System.out.println(lege.compareTo(lege));
-        Lege[] leger = {spesialist, lege};
+        System.out.println(lege.compareTo(lege2));
+        System.out.println("\n");
+        Lege[] leger = {spesialist, lege, lege2};
         System.out.println(leger[0]);
         System.out.println(leger[1]);
+        System.out.println(leger[2]);
+        System.out.println("\nSortert liste:");
         Arrays.sort(leger);
         System.out.println(leger[0]);
         System.out.println(leger[1]);
+        System.out.println(leger[2]);
         //tester skrivResept
-        System.out.println("\ntester skrivResept");
+        System.out.println("\ntester skrivResept:");
         spesialist.skrivHvitResept(vanlig, pasient, 3);
         spesialist.skrivHvitResept(vanedannende, pasient, 3);
         spesialist.skrivBlaaResept(narkotisk, pasient, 3);
