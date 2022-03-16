3f8c4b2c9d7aec332a0e1418abb52662212f86d9
Ingeridpus <ingeridb.johansen@yahoo.com>
2022-03-16 13:46:32 +0100

    Startet på legesystem

--- Legesystem.java
+++ Legesystem.java
@@ -1,9 +1,46 @@
-import java.io.File;
-import java.util.Scanner;
 
-public class Legesystem {
-    private static void LesFraFil(String filnavn) {
-        File fil = new File(filnavn);
-        Scanner sc;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+class Legesystem{
+
+  public Legesystem(String filnavn){
+    File f = new File(filnavn);
+    Scanner sc = null;
+
+    try {
+      sc = new Scanner(f);
+    } catch (FileNotFoundException e) {
+      System.out.println("File '" + filnavn + "' not found!");
     }
+    
+    
+    while (sc.hasNextLine()){
+      String[] filen = sc.nextLine().split(" ");
+
+      if (filen[1].compareTo("Pasienter") == 0){
+        while (sc.hasNextLine()){
+          filen = sc.nextLine();
+          if (filen.charAt(0) == "#"){
+            break;
+          }
+          //legg til pasient 
+
+        }
+      }
+
+
+      else if (filen[1].compareTo("Legemidler") == 0){
+        while (sc.hasNextLine()){
+          filen = sc.nextLine();
+          if (filen.charAt(0) == "#"){
+            break;
+          }
+          //legg til legemiddel
+
+        }
+      }
+    }
+  }
 }
